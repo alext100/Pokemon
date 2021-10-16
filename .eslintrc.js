ee06b3f0b0d9b835ef7f59803711a017d523a0df
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-16 16:33:54 +0200

    map() and forEach() to render API response to getPokemons() added

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   parser: "@babel/eslint-parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
     "no-param-reassign": "off",
--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,28 @@
-import PokemonServices from "./PokemonServices.js";
-import Pokemon from "./Pokemon.js";
+import PokemonServices from './PokemonServices.js';
+import Pokemon from './Pokemon.js';
 
-const services = new PokemonServices;
-const urlAPI = "https://pokeapi.co/api/v2/pokemon/4";
-
-const parentElement = document.querySelector(".album-container");
+const services = new PokemonServices();
+const parentElement = document.querySelector('.album-container');
+const urlAPI = 'https://pokeapi.co/api/v2/pokemon?limit=9&offset=9';
+let arrayOfURLs = [];
 
 (async () => {
   try {
     const response = await services.getPokemons(urlAPI);
-    console.log('response: ', response);
-    const urlOfImage = response.sprites.other.dream_world.front_default;
-    const pokemon = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon2 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon3 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon4 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon5 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon6 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon7 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon8 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-    const pokemon9 = new Pokemon(parentElement, urlAPI, urlOfImage, response.height, response.weight, response.name);
-  }
-  catch (error) {
+    arrayOfURLs = response.results;
+    arrayOfURLs = arrayOfURLs.map((result) => result.url);
+    console.log('arryOfURLs: ', arrayOfURLs);
+    arrayOfURLs.forEach(async (urlPokemon) => {
+      const response = await services.getPokemons(urlPokemon);
+      console.log('response: ', response);
+      const urlOfImage = response.sprites.other.dream_world.front_default;
+      new Pokemon(parentElement, urlPokemon, urlOfImage, response.height, response.weight, response.name);
+    });
+  } catch (error) {
     console.log(error);
   }
 })();
+
+
+
+
