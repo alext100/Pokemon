6fd9a1f57945b0d0c49f9684ee501951d330c75c
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-17 01:34:38 +0200

    showPokemonsOnPage function added

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,40 @@
+import paginate from './jpaginate.js';
 import PokemonServices from './PokemonServices.js';
 import Pokemon from './Pokemon.js';
 
 const services = new PokemonServices();
+
 const parentElement = document.querySelector('.album-container');
-const urlAPI = 'https://pokeapi.co/api/v2/pokemon?limit=9&offset=9';
+const paginationBlock = document.querySelector('.pagination');
+const previousButton = document.querySelector('.pagination--previous-button');
+const firstPageInPagination = document.querySelector('.pagination--first-page');
+const secondPageInPagination = document.querySelector('.pagination--second-page');
+const thirdPageInPagination = document.querySelector('.pagination--third-page');
+
+const offset = 0;
+const urlAPI = `https://pokeapi.co/api/v2/pokemon?limit=1200&offset=${offset}`;
 let arrayOfURLs = [];
+
+const showPokemonsOnPage = ({ currentPage, startIndex, endIndex }) => {
+  const urlOfPokemonsToShowOnPage = arrayOfURLs.slice(startIndex, endIndex + 1);
+  showActivePageNamberInPaginationBlock({ currentPage });
+  urlOfPokemonsToShowOnPage.forEach(async (urlPokemon) => {
+    const response = await services.getPokemons(urlPokemon);
+    const urlOfImage = response.sprites.other.dream_world.front_default;
+    new Pokemon(parentElement, urlPokemon, urlOfImage, response.height, response.weight, response.name);
+  });
+}
 
 (async () => {
   try {
     const response = await services.getPokemons(urlAPI);
     arrayOfURLs = response.results;
     arrayOfURLs = arrayOfURLs.map((result) => result.url);
-    console.log('arryOfURLs: ', arrayOfURLs);
-    arrayOfURLs.forEach(async (urlPokemon) => {
-      const response = await services.getPokemons(urlPokemon);
-      console.log('response: ', response);
-      const urlOfImage = response.sprites.other.dream_world.front_default;
-      new Pokemon(parentElement, urlPokemon, urlOfImage, response.height, response.weight, response.name);
-    });
+    const paginationData = paginate(arrayOfURLs.length);
+    showPokemonsOnPage(paginationData);
+    return paginationData;
   } catch (error) {
     console.log(error);
   }
 })();
-
-
-
 
