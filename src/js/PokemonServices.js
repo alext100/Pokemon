936ac9a6c78fc246fc0efc55c8bb3925945c7e97
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-16 11:52:45 +0200

    CreatePokemon method added

--- src/js/PokemonServices.js
+++ src/js/PokemonServices.js
@@ -1,11 +1,24 @@
 class PokemonServices {
   urlAPI = "https://pokeapi.co/api/v2/pokemon?limit=9&offset=0";
 
-  async getPokemons() {
-    const response = await fetch(this.urlAPI);
+  async getPokemons(urlAPI) {
+    const response = await fetch(urlAPI);
     const pokemons = await response.json();
     return pokemons;
   }
+
+  async createPokemon(pokemon) {
+    const response = await fetch(`${this.urlAPI}`, {
+      method: "POST",
+      body: JSON.stringify(pokemon),
+      header: {
+        "Content-Type": "application/json",
+      },
+    });
+    const newPokemon = await response.json();
+    return newPokemon;
+  }
+
 
 }
 
