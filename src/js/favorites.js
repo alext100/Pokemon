57b3251d442828fdb6191f27274015e4e6c8d9b0
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-17 13:23:27 +0200

    Comments and console.log()'s was deleted

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,15 +1,9 @@
 import PokemonServices from './PokemonServices.js';
 import Pokemon from './Pokemon.js';
-// import { showPokemonsOnPage } from './index.js'
 
 const services = new PokemonServices();
 
 const parentElement = document.querySelector('.album-container');
-const paginationBlock = document.querySelector('.pagination');
-const previousButton = document.querySelector('.pagination--previous-button');
-const firstPageInPagination = document.querySelector('.pagination--first-page');
-const secondPageInPagination = document.querySelector('.pagination--second-page');
-const thirdPageInPagination = document.querySelector('.pagination--third-page');
 
 const urlHerokuAPI = "http://pokemon-api-aleksandr.herokuapp.com/pokemon";
 let arrayOfFavorites = [];
